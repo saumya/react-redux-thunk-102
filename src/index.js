178bc555694b095b5f53bbd8e99d7e8a3fbc7100
saumya <2saumyaray@gmail.com>
2016-05-04 15:35:02 +0530

    code cleanup

--- src/index.js
+++ src/index.js
@@ -3,24 +3,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import AllInOneReducer from './reducers'
-
 import thunk from 'redux-thunk'
 
+import AllInOneReducer from './reducers'
 import ApplicationContainer from './containers/ApplicationContainer'
-import initialStateUtil, {getInitialState,generateTree} from './util/initialStateUtil'
+import initialStateUtil, {getInitialState} from './util/initialStateUtil'
 
-//
-//var store = createStore(reducer, initialState,applyMiddleware);
-// or
-//var store = createStore(reducer,applyMiddleware);
-//
 let store = createStore(AllInOneReducer,getInitialState(),applyMiddleware(thunk));
-// just a check
-generateTree();
-
 const rootEL = document.getElementById('react-app');
-
 render(
   <Provider store={store}>
     <ApplicationContainer />
