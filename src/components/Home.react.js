e07b58750e0b9cf60b4c68535d64511393b97726
saumya <2saumyaray@gmail.com>
2016-05-04 20:00:48 +0530

    added login component

--- src/components/Home.react.js
+++ src/components/Home.react.js
@@ -1,11 +1,13 @@
 //
 'use strict';
 import React, {Component} from 'react'
+import LoginContainer from '../containers/LoginContainer'
 class Home extends Component {
   render(){
     return (
       <div>
         <div>Home</div>
+        <LoginContainer />
       </div>
     );
   }
