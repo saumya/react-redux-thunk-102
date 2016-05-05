f558a4cc7975bb2bded7c583f908adde67c08cf3
saumya <2saumyaray@gmail.com>
2016-05-05 12:41:08 +0530

    learning to get the data back to components

--- src/components/ApplicationControllerView.react.js
+++ src/components/ApplicationControllerView.react.js
@@ -3,11 +3,17 @@
 import React, {Component} from 'react'
 import { Link, IndexLink } from 'react-router'
 
+import { connect } from 'react-redux'
+
 import Nav from './Nav.react'
 import Menu from './Menu.react'
 
 class ApplicationControllerView extends Component {
   render(){
+    console.group('ApplicationControllerView : render');
+    console.log('this.props',this.props);
+    console.log('this.state',this.state);
+    console.groupEnd();
     return (
       <div>
         <div>Application Controller View : Root Controller View</div>
@@ -17,4 +23,24 @@ class ApplicationControllerView extends Component {
     );
   }
 }
+// Bind to the events of the Store
+const mapStateToProps = (state, ownProps) => {
+  console.log('ApplicationControllerView : mapStateToProps');
+  return {
+    //active: ownProps.filter === state.visibilityFilter
+    state
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  console.log('ApplicationControllerView : mapDispatchToProps');
+  return {
+    onClick: () => {
+      //dispatch(setVisibilityFilter(ownProps.filter))
+      console.log('ApplicationControllerView : onClick');
+    }
+  }
+}
+ApplicationControllerView = connect(mapStateToProps,mapDispatchToProps)(ApplicationControllerView)
+
 export default ApplicationControllerView;
